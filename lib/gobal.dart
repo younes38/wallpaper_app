277705ffe149af14bb3dea41efd3ec09d2fb4a7b
Mimene Younes <43863709+younes38@users.noreply.github.com>
2020-12-10 12:35:47 +0100

    Update gobal.dart

--- lib/gobal.dart
+++ lib/gobal.dart
@@ -1,6 +1,7 @@
 class Global {
   static const apiURL = "https://api.pexels.com/v1/search";
+  // use your own token
   static const token =
-      '563492ad6f91700001000001a9787a0be63044d7bf113bb6558e9fbc';
+      '';
   static const per_page = 15;
 }
